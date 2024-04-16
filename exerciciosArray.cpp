652af223b3e47e071c5e371921807ca3480807f5
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-16 00:48:28 +0000

    String e Array

--- exerciciosArray.cpp
+++ exerciciosArray.cpp
@@ -5,23 +5,40 @@ int array01(){
 
     //Escreva um programa que solicita ao usuário que digite 5 números inteiros e armazene-os em um array. Em seguida, imprima os números na ordem inversa.
 
-    
-    cout << "Digite 5 números: ";
-    cin >> ;
+    int array[5];
 
+    cout << "Digite 5 números inteiros: ";
+    cin >> array[0] >> array[1] >> array[2] >> array[3] >> array[4];
+    cout << "Você digitou: " << array[4] << array[3] << array[2] << array[1] << array[0] <<std::endl;
 
     return 0;
 }
 
 
-//TEM QUE FAZER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+int array02(){
 
+    //Crie um programa que leia 3 números inteiros do usuário e os armazene em um array. Em seguida, solicite ao usuário que digite um índice e imprima o número correspondente no array.
 
+    int array[3], indice;
 
+    cout << "Digite 3 número inteiros: ";
+    cin >> array[0] >> array[1] >> array[2];
 
+    cout << "Digite o índice: ";
+    cin >> indice;
 
+    if(indice == 0){
+        cout << "Elemento: " << array[0] << std::endl;
+    } else if(indice == 1){
+        cout << "Elemento: " << array[1] << std::endl;
+    } else if(indice == 2){
+        cout << "Elemento: " << array[2] << std::endl;
+    } else{
+        cout << "Elemento não encontrado!" << std::endl;
+    }
 
-
+    return 0;
+}
 
 
 
@@ -34,8 +51,8 @@ int array01(){
 
 
 int main(){
-    array01();
-    //array02();
+    //array01();
+    array02();
     //array03();
 
 
